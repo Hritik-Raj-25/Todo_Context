e4ffe1124302f3751439e8efbdc1d3f07e1f0785
Hritik Raj <hritikraj0203@gmail.com>
2024-11-11 23:54:27 +0530

    todo component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import './App.css'
 
 function App() {
 
+  
+
   return (
+    
     <div className="bg-[#20252d] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
